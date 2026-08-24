90695bccac2f619b6316d241651203adddd48f69
NinjaFromConsole <rothtyler24@gmail.com>
2026-08-23 18:52:43 -0700

    Door Rotate + HUD
    - Added a HUD frame around the camera
    - Doors now rotate open when clicked
    - Fixed being able to open a door while its open animation is playing

--- Assets/Scripts/Objects/Door.cs
+++ Assets/Scripts/Objects/Door.cs
@@ -1,16 +1,32 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class Door : ClickableObject
 {
     [Header("References")]
     [SerializeField] private Sprite[] doorNumbers;
+    [SerializeField] private Transform doorSpriteTransform;
     [SerializeField] private SpriteRenderer doorNumbersRenderer;
+
+    [Header("Parameters")]
+    [SerializeField] private float doorRotateTime;
+    [SerializeField] private float doorRotateAngle;
+
+    private bool open; 
 
     protected override void OnMouseDown()
     {
+        if (open)
+        {
+            return;
+        }
+
         AudioManager.Instance.PlayOneShot(FmodEvents.Instance.openDoor, transform.position);
         MainCameraMove.Instance.MoveCamera(transform.position);
+        open = true;
+
+        CoroutineManager.Instance.Run(RotateDoor(doorRotateAngle));
     }
 
     protected override void OnMouseUp()
@@ -26,4 +42,21 @@ public class Door : ClickableObject
         }
         doorNumbersRenderer.sprite = doorNumbers[number];
     }
+
+    private IEnumerator RotateDoor(float rotateAmount)
+    {
+        Vector3 startRotation = doorSpriteTransform.eulerAngles;
+        Vector3 finalRotation = new Vector3(doorSpriteTransform.eulerAngles.x, doorSpriteTransform.eulerAngles.y + rotateAmount, doorSpriteTransform.eulerAngles.z);
+        
+        float i = 0;
+        while (i < doorRotateTime)
+        {
+            doorSpriteTransform.eulerAngles = Vector3.Lerp(startRotation, finalRotation, Mathf.SmoothStep(0, 1, i / doorRotateTime));
+            yield return null;
+            i += Time.deltaTime;
+        }
+
+        doorSpriteTransform.eulerAngles = finalRotation;
+    }
+    
 }
