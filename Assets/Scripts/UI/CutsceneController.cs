849a67c29c48013bdd9c2a9e6b7b71e412f5f7c2
JacktheSeal <134016721+JacktheSeal@users.noreply.github.com>
2026-08-28 00:54:35 -0700

    Test Cutscene Implemented
    Implemented Test Cutscene

---
+++ Assets/Scripts/UI/CutsceneController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class CutsceneController : MonoBehaviour
+{
+    [SerializeField] private TMP_Text dialogueText;
+    [SerializeField] private string[] dialogueLines;
+
+    private int currentLine = 0;
+
+    private void Start()
+    {
+        dialogueText.text = dialogueLines[currentLine];
+    }
+
+    private void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame ||
+            Mouse.current.rightButton.wasPressedThisFrame)
+        {
+            NextLine();
+        }
+    }
+
+    private void NextLine()
+    {
+        currentLine++;
+
+        if (currentLine >= dialogueLines.Length)
+        {
+            EndCutscene();
+            return;
+        }
+
+        dialogueText.text = dialogueLines[currentLine];
+    }
+
+    private void EndCutscene()
+    {
+        gameObject.SetActive(false);
+    }
+}
